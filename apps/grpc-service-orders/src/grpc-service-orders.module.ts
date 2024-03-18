3ccbf769930f0b838ebc60185f6c6d7160132f36
Ujjwal Aryal <ujjwal.a@iqzsystems.com>
2024-03-18 17:26:41 +0530

    Bulk commit: added 3 microservices and libs/common

--- apps/grpc-service-orders/src/grpc-service-orders.module.ts
+++ apps/grpc-service-orders/src/grpc-service-orders.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { GrpcServiceOrdersController } from './grpc-service-orders.controller';
-import { GrpcServiceOrdersService } from './grpc-service-orders.service';
 
 @Module({
   imports: [],
-  controllers: [GrpcServiceOrdersController],
-  providers: [GrpcServiceOrdersService],
+  controllers: [],
+  providers: [],
 })
 export class GrpcServiceOrdersModule {}
---
+++ apps/grpc-service-users/src/users/users.service.ts
@@ -0,0 +1,74 @@
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import {
+  CreateUserDto,
+  PaginationDto,
+  UpdateUserDto,
+  User,
+  Users,
+} from '@app/common';
+import { randomUUID } from 'crypto';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private readonly users: User[] = [];
+
+  onModuleInit() {
+    this.create({ name: 'Ujjwal', age: 25 });
+    this.create({ name: 'Light', age: 24 });
+  }
+
+  create(createUserDto: CreateUserDto) {
+    const newUser: User = {
+      ...createUserDto,
+      id: randomUUID(),
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findAll(): Users {
+    return { users: this.users };
+  }
+
+  findOne(id: string): User {
+    return this.users.find((user) => user.id === id);
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+      return this.users[userIndex];
+    }
+    throw new NotFoundException(`User not found by id ${id}`);
+  }
+
+  remove(id: string) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      return this.users.splice(userIndex)[0];
+    }
+    throw new NotFoundException(`User not found by id ${id}`);
+  }
+
+  queryUsers(
+    paginationDtoStream: Observable<PaginationDto>,
+  ): Observable<Users> {
+    const subject = new Subject<Users>();
+
+    const onNext = (paginationDto: PaginationDto) => {
+      const start = paginationDto.page * paginationDto.skip;
+      subject.next({
+        users: this.users.slice(start, start + paginationDto.skip),
+      });
+    };
+
+    const onComplete = () => subject.complete;
+    paginationDtoStream.subscribe({
+      next: onNext,
+      complete: onComplete,
+    });
+    return subject.asObservable();
+  }
+}
